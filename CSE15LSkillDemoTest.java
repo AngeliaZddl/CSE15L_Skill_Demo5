efc02f2ecb19101f179a6514a371d744accb3b1f
AngeliaZddl <78582332+AngeliaZddl@users.noreply.github.com>
2022-05-01 23:52:38 -0700

    fix mis

--- CSE15LSkillDemoTest.java
+++ CSE15LSkillDemoTest.java
@@ -3,7 +3,7 @@ import org.junit.*;
 public class CSE15LSkillDemoTest {
     @Test
     public void skillDemoTest() {
-        String exp = "Hello World"; //fail
+        String exp = "Hello World!"; //fail
         String act = CSE15LSkillDemo.combString("Hello", "World");
         assertEquals(act, exp);
     }
